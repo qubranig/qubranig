7cf2c228849febb5ee342c498c292c5545882c62
Kubriqz <46535970+Kubriqz@users.noreply.github.com>
2019-07-23 20:26:23 +0200

    dsa

--- zudumm/Program.cs
+++ zudumm/Program.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 
 namespace zudumm
 {
+    class Testklasse
+    {
+        private int zahl1 {get; set; }
+        public int gekapselte_zahl1 { get => zahl1; set => zahl1 = value; }
+
+        public Testklasse(int a)
+            {
+            zahl1 = a;
+            }
+        public Testklasse()
+        {
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Testklasse a = new Testklasse();
+            a.gekapselte_zahl1 = 1;
+            Testklasse b = new Testklasse(2);
+            
+
+            Console.WriteLine(b.gekapselte_zahl1);
+            Console.WriteLine(a.gekapselte_zahl1);
+            Console.ReadKey();
         }
     }
 }
