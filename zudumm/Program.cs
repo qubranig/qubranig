18ece7ad67c8ff9321f8376d44a001c2f522e6f5
Kubriqz <46535970+Kubriqz@users.noreply.github.com>
2019-07-23 21:04:31 +0200

    adas

--- zudumm/Program.cs
+++ zudumm/Program.cs
@@ -27,10 +27,18 @@ namespace zudumm
             Testklasse a = new Testklasse();
             a.gekapselte_zahl1 = 1;
             Testklasse b = new Testklasse(2);
-            
-
             Console.WriteLine(b.gekapselte_zahl1);
             Console.WriteLine(a.gekapselte_zahl1);
+
+            Console.WriteLine("---------------------------------------------------------------");
+            Random randy = new Random();
+            int i=0;
+            while (i<20)
+            {
+                i++;
+                Console.WriteLine(randy.Next(0,3));
+            }
+
             Console.ReadKey();
         }
     }
